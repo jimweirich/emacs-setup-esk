f1797c79f302af7ae5b11e9c6b817fb5e11a961d
Jim Weirich <jim.weirich@gmail.com>
2011-08-17 21:25:51 -0400

    Put pkg and local directorys on the load path

---
+++ 00-load-path.el
@@ -0,0 +1,5 @@
+(setq pkgs-package-dir (concat user-specific-dir "/pkgs"))
+(add-to-list 'load-path pkgs-package-dir)
+
+(setq local-package-dir (concat user-specific-dir "/local"))
+(add-to-list 'load-path local-package-dir)
