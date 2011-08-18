c7a854031b418c73099bbae18ef9d8cf056f941c
Jim Weirich <jim.weirich@gmail.com>
2011-08-17 21:25:30 -0400

    Add debugging tool

---
+++ dbg.el
@@ -0,0 +1,16 @@
+;;; ELisp Debugging aids
+;;; ----------------------------------------------------------------------
+
+;;; Write debugging messages to *DBG*.
+(defun dbg (&rest msgs)
+  (let ((buf (current-buffer)))
+    (set-buffer (get-buffer-create "*dbg*"))
+    (goto-char (point-max))
+    (insert (apply 'concat
+                   (mapcar (lambda (a) (format "%s" a)) msgs) ))
+    (insert "\n")
+    (set-buffer buf)))
+
+(defun dbg-inline (message result)
+  (dbg message result)
+  result)
