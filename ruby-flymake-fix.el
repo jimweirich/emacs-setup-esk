5d9bb773c784ebd12e293717f6ae455a2a0f31e7
Jim Weirich <jim.weirich@gmail.com>
2011-08-22 16:05:00 -0400

    Use Ruby 1.9 for flymake

---
+++ ruby-flymake-fix.el
@@ -0,0 +1,10 @@
+;;; Override the starter kits ruby flymake init to use Ruby 1.9
+
+(defun flymake-ruby-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    ;; Invoke ruby with '-c' to get syntax checking
+    (list "ruby19" (list "-c" local-file))))
