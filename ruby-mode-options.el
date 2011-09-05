e0e72e6dac2d1980b418801b3ee1b8dfe43249e2
Jim Weirich <jim.weirich@gmail.com>
2011-09-05 13:43:08 -0400

    Ruby-mode enhancements.
    * Auto delete trailing spaces
    * Don't use deep nesting.

---
+++ ruby-mode-options.el
@@ -0,0 +1,15 @@
+;;; Options for customizing Ruby model
+
+(setq ruby-deep-indent-paren-style nil)
+
+
+;;; Delete trailing spaces
+
+(add-hook 'ruby-mode-hook '(lambda ()
+                             (add-hook
+                              (cond ((boundp 'before-save-hook)
+                                     (make-local-variable 'before-save-hook)
+                                     'before-save-hook)
+                                    ((boundp 'write-contents-functions) 'write-contents-functions)
+                                    ((boundp 'write-contents-hooks) 'write-contents-hooks))
+                              'delete-trailing-whitespace)))
