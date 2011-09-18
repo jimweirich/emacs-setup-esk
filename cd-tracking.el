a6ee509920afcd57225f40eae4a73bd7c9e9943c
Jim Weirich <jim.weirich@gmail.com>
2011-09-17 22:04:22 -0400

    Add support for $PATH tracking from shell.

--- cd-tracking.el
+++ cd-tracking.el
@@ -4,10 +4,16 @@
   (shell-cd dir)
   (message dir) )
 
+(defun jw-set-path (path)
+  (setq exec-path (split-string path ":")))
+
 (defun jw-track-go-directories (string)
   (let ((out (replace-regexp-in-string "\n$" "" string)))
-    (if (string-match "\\[cd: \\(.*\\)\\]" out)
-        (let ((dir (substring out (match-beginning 1) (match-end 1))))
-          (jw-cd dir) ))))
+    (cond ((string-match "\\[cd: \\(.*\\)\\]" out)
+           (let ((dir (substring out (match-beginning 1) (match-end 1))))
+             (jw-cd dir) ))
+          ((string-match "\\[path: \\(.*\\)\\]" out)
+           (let ((path (substring out (match-beginning 1) (match-end 1))))
+             (jw-set-path path))))))
 
 (add-hook 'comint-output-filter-functions 'jw-track-go-directories)
