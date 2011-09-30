a25be8c7e4c67a4bcc0803680adcfee745612208
Jim Weirich <jim.weirich@gmail.com>
2011-09-30 10:08:51 -0400

    Added actionscript mode

--- auto.el
+++ auto.el
@@ -1,2 +1,14 @@
 
-(autoload 'jw-align         "jw-align"         "Alignment functionality" t)
+(autoload 'jw-align           "jw-align"          "Alignment functionality" t)
+(autoload 'actionscript-mode  "actionscript-mode" "Actionscript programming mode" t)
+
+(defun make-auto (pattern mode)
+  "Add a pattern to the auto-mode alist."
+  (let ((ans (assoc pattern auto-mode-alist)))
+    (if (and ans (equal mode (cdr ans)))
+	(print "Do Nothing")
+      (print "Added")
+      (setq auto-mode-alist
+	    (cons (cons pattern mode) auto-mode-alist)))))
+
+(make-auto "\\.as$"    'actionscript-mode)
