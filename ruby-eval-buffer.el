a06a5d15fd66788428567d02a02c833086dd39b9
Jim Weirich <jim.weirich@gmail.com>
2011-10-08 12:01:35 -0400

    Add error message highlighting to ruby-eval-buffer.

--- ruby-eval-buffer.el
+++ ruby-eval-buffer.el
@@ -1,12 +1,28 @@
+(defconst jw-ruby-eval-command "/Users/jim/local/bin/ruby19")
+
 (defun ruby-eval-buffer ()
   "Evaluate the current buffer and display the result in a buffer."
   (interactive)
   (save-buffer)
   (let* ((file-name (buffer-file-name (current-buffer)))
-         (command (concat "ruby19 " file-name)))
+         (command (concat jw-ruby-eval-command " " file-name)))
     (shell-command-on-region (point-min) (point-max) command "*RUBY*" nil)
     (pop-to-buffer "*RUBY*")
-    (other-window 1)))
+    (other-window 1)
+    (ruby-eval-pretty-up-errors "*RUBY*")
+    (message "..")))
+
+(defun ruby-eval-pretty-up-errors (buffer)
+  "Fix up the buffer to highlight the error message (if it contains one)."
+  (save-excursion
+    (set-buffer "*RUBY*")
+    (goto-char (point-min))
+    (let ((pos (search-forward-regexp "\\.rb:[0-9]+:\\(in.+:\\)? +" (point-max) t)))
+      (if pos (progn
+                (goto-char pos)
+                (insert-string "\n\n")
+                (end-of-line)
+                (insert-string "\n"))))))
 
 (defun ruby-clear-eval-buffer ()
   (interactive)
