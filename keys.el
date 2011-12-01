f0c471a62c8c4b53612dcb147a0d4d38a249f210
Jim Weirich <jim.weirich@gmail.com>
2011-12-01 11:06:06 -0500

    Add C-C > for resetting the tags table.

--- keys.el
+++ keys.el
@@ -75,6 +75,8 @@
 (global-set-key (kbd "C-C t 1") (lambda () (interactive) (setq jw-test-single-window t))) ; pkg:testing
 (global-set-key (kbd "C-C t 2") (lambda () (interactive) (setq jw-test-single-window nil))) ; pkg:testing
 
+(global-set-key (kbd "C-C >") 'tags-reset-tags-tables)
+
 (global-set-key (kbd "C-z") 'scroll-down) ; I *hate* suspend bound on this key
 (global-set-key (kbd "C-C SPC") 'jw-shell) ; override the default binding here
 (global-set-key (kbd "C-C \\")  'codol-toggle)
