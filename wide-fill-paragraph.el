b6a8d2f6cc82a72222e8caedcaae61713157bcdb
Jim Weirich <jim.weirich@gmail.com>
2012-01-31 11:11:12 -0500

    Add wide-fill-paragraph.

---
+++ wide-fill-paragraph.el
@@ -0,0 +1,17 @@
+;;; Wide fill paragraph function
+
+(defun jw-wide-fill-paragraph ()
+  "Fill a paragraph with an extremely wide fill column."
+  (interactive)
+  (let ((fill-column 1000000000))
+    (fill-paragraph)))
+
+(defun jw-fill-paragraph (arg)
+  "Wrapping function that will either call the normal fill 
+paragraph, or the wide version."
+  (interactive "P")
+  (if (null arg)
+      (fill-paragraph)
+    (jw-wide-fill-paragraph)))
+
+(global-set-key (kbd "M-q") 'jw-fill-paragraph)
