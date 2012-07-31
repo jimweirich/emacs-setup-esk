595dfacd08ae3cc2be4f0cf5ef971d94dff78cb1
Jim Weirich <jim.weirich@gmail.com>
2012-07-31 16:04:30 -0400

    Create fallback for ruby-flymake mode if no autoruby present.

--- ruby-flymake-fix.el
+++ ruby-flymake-fix.el
@@ -2,6 +2,9 @@
 
 (setq flymake-ruby-command-name (concat user-specific-dir "/bin/autoruby"))
 
+(if (not (file-exists-p flymake-ruby-command-name))
+    (setq flymake-ruby-command-name "ruby"))
+
 (defun flymake-ruby-init ()
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
                      'flymake-create-temp-inplace))
