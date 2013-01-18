49c0c08a2614e7d24b3f43ee58678a20a40281a6
Jim Weirich <jim.weirich@gmail.com>
2013-01-18 11:14:42 -0500

    Leave menu mode off.

--- menu-bar-mode.el
+++ menu-bar-mode.el
@@ -1,3 +1,9 @@
 ;; Turn the menu bar back on
 
-(menu-bar-mode)
+
+;;; Leaving it off for now.
+;;; Doesn't seem to effect anything in window mode,
+;;; and not wanted in terminal mode.
+
+;;; (menu-bar-mode)
+
