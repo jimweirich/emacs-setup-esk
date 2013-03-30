eb790c6c2a7c790f6486804595e7e60a52c823fb
Jim Weirich <jim.weirich@gmail.com>
2013-03-30 01:29:36 -0400

    Flatten snippet directory

--- yasnippet-options.el
+++ yasnippet-options.el
@@ -6,13 +6,35 @@
 (require 'yasnippet)
 
 (setq snippets-dir (concat user-specific-dir "/snippets"))
-(setq yas/root-directory (cons snippets-dir yas/root-directory))
-(yas/reload-all)
 
 (defun jw-snippets ()
   (interactive)
   (setq yas/root-directory (list snippets-dir))
   (yas/reload-all))
+
+(defun std-snippets ()
+  (interactive)
+  (setq yas/root-directory (cons snippets-dir yas/root-directory))
+  (yas/reload-all))
+
+(if (string= user-login-name "jim")
+    (jw-snippets)
+  (std-snippets))
+
+;;; Should snippets minimize parens where possible?
+(setq tm_minimize_paren t)
+
+(defun snippet-paren-start ()
+  (if tm_minimize_paren " " "("))
+
+(defun snippet-paren-end ()
+  (if tm_minimize_paren "" ")"))
+
+(defun snippet-remove-empty-parens (text)
+  (if (string-equal text "()") "()" text) )
+
+(mapc '(lambda (hook) (add-hook hook 'yas/minor-mode-on))
+      '(ruby-mode-hook))
 
 ;;; The default background tends to wash out the foreground color a
 ;;; bit.  Let's try something different.
