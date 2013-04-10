df2aaa93d49abbb6dbdfa2769ebaa93231eb70df
Jim Weirich <jim.weirich@gmail.com>
2013-04-10 18:14:59 -0400

    Look for git executable.

--- magit-options.el
+++ magit-options.el
@@ -2,4 +2,9 @@
 
 (add-to-list 'load-path (concat user-specific-dir "/pkgs/magit/share/emacs/site-lisp"))
 (autoload 'magit-status "magit" nil t)
-(setq magit-git-executable "/usr/local/bin/git")
+(cond ((file-readable-p "/usr/local/bin/git")
+       (setq magit-git-executable "/usr/local/bin/git"))
+      ((file-readable-p "/usr/bin/git")
+       (setq magit-git-executable "/usr/bin/git"))
+      (t (setq magit-git-executable "git")))
+"/usr/local/bin/git"
