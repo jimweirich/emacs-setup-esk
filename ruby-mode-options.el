a5cfaf16787441d93727c0a1db1a52cb9a4867dd
Jim Weirich <jim.weirich@gmail.com>
2013-07-18 14:03:26 -0400

    Ruby mode now reindents line where c-j was used.

--- ruby-mode-options.el
+++ ruby-mode-options.el
@@ -16,3 +16,16 @@
               'delete-trailing-whitespace)))
 
 (add-hook 'ruby-mode-hook '(lambda () (auto-fill-mode -1)))
+
+(defun jw-reindent-then-newline-and-indent ()
+  (interactive "*")
+  (newline)
+  (save-excursion
+    (end-of-line 0)
+    (indent-according-to-mode)
+    (delete-region (point) (progn (skip-chars-backward " \t") (point))))
+  (indent-according-to-mode))
+
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (define-key ruby-mode-map "\C-j" 'jw-reindent-then-newline-and-indent)))
