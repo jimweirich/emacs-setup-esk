6ea999ef856fb9e9d813b86a3f99dc9dbe225a32
Jim Weirich <jim.weirich@gmail.com>
2013-10-11 21:22:24 -0400

    Set user defined directories

--- 00-load-path.el
+++ 00-load-path.el
@@ -1,3 +1,6 @@
+(setq user-specific-dir esk-user-dir)
+;;; (mapc 'load-file (file-expand-wildcards (concat (expand-file-name esk-user-dir) "/*.el")))
+
 (setq pkgs-package-dir (concat user-specific-dir "/pkgs"))
 (add-to-list 'load-path pkgs-package-dir)
 
