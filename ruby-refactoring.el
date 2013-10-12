b22da4fc75d748acf1ceb5ed409d9d3dae19a89b
Jim Weirich <jim.weirich@gmail.com>
2013-10-11 22:14:44 -0400

    Extract common code for refactorings

--- ruby-refactoring.el
+++ ruby-refactoring.el
@@ -11,34 +11,43 @@
 ;;; will be inserted and runs the generic insert function
 ;;; (rrf-insert-extraction, generally bound to "C-Cay").
 
-;;; Global used by the refactorings
+;;; Globals used by the refactorings
 
+(defvar rrf-insertion-method nil)
 (defvar rrf-refactored-const-body nil)
 (defvar rrf-refactored-const-name nil)
 (defvar rrf-refactored-method-body nil)
 (defvar rrf-refactored-method-name nil)
-(defvar rrf-insertion-method nil)
+(defvar rrf-refactored-var-body nil)
+(defvar rrf-refactored-var-name nil)
 
 ;;; Top-level refactoring functions
+
+(defun rrf-extract-temporary (var-name beg end)
+  "Extract an expression into a temporary variable."
+  (interactive "sVariable Name: \nr")
+  (setq rrf-refactored-var-name var-name)
+  (setq rrf-refactored-var-body (buffer-substring beg end))
+  (rrf-replace-region beg end rrf-refactored-var-name)
+  (setq rrf-insertion-method 'rrf-insert-extracted-temporary))
+
+(defun rrf-insert-extracted-temporary ()
+  "Insert the definition for a previously extracted temporary variable."
+  (interactive)
+  (rrf-insert-named-expression rrf-refactored-var-name rrf-refactored-var-body))
 
 (defun rrf-extract-constant (const-name beg end)
   "Extract an expression into a constant."
   (interactive "sConstant Name: \nr")
   (setq rrf-refactored-const-name const-name)
   (setq rrf-refactored-const-body (buffer-substring beg end))
-  (kill-region beg end)
-  (insert-string rrf-refactored-const-name)
-  (indent-according-to-mode)
+  (rrf-replace-region beg end rrf-refactored-const-name)
   (setq rrf-insertion-method 'rrf-insert-extracted-constant))
 
 (defun rrf-insert-extracted-constant ()
   "Insert the definition for a previously extracted constant."
   (interactive)
-  (insert-string rrf-refactored-const-name)
-  (indent-according-to-mode)
-  (insert-string " = ")
-  (insert-string rrf-refactored-const-body)
-  (indent-region (mark) (point)))
+  (rrf-insert-named-expression rrf-refactored-const-name rrf-refactored-const-body))
 
 (defun rrf-extract-method (method-name args beg end)
   "Extract a method."
@@ -47,13 +56,10 @@
       (setq rrf-refactored-method-name method-name)
     (setq rrf-refactored-method-name
           (concat method-name "(" args ")")))
-  (setq beg (rrf-adj-beg beg end))
-  (setq end (rrf-adj-endl beg end))
+  (setq beg (rrf-adj-beg beg end)
+        end (rrf-adj-endl beg end))
   (setq rrf-refactored-method-body (buffer-substring beg end))
-  (goto-char beg)
-  (kill-region beg end)
-  (insert-string rrf-refactored-method-name)
-  (indent-according-to-mode)
+  (rrf-replace-region beg end rrf-refactored-method-name)
   (setq rrf-insertion-method 'rrf-insert-extracted-method))
 
 (defun rrf-insert-extracted-method ()
@@ -80,6 +86,21 @@
 
 ;;; Utility Functions
 
+(defun rrf-replace-region (beg end replacement)
+  "Replace the region with REPLACEMENT"
+  (goto-char beg)
+  (kill-region beg end)
+  (insert-string replacement)
+  (indent-according-to-mode))
+
+(defun rrf-insert-named-expression (name expression)
+  "Insert NAME = EXPRESSION."
+  (insert-string name)
+  (indent-according-to-mode)
+  (insert-string " = ")
+  (insert-string expression)
+  (indent-region (mark) (point)))
+
 (defun rrf-adj-beg (beg end)
   "Get the adjusted beginning of the region."
   (if (< beg end) beg end))
@@ -97,9 +118,10 @@
 
 (defun rrf-first-column-p (loc)
   "Is LOC in the first column?"
-  (save-excursion
-    (goto-char (- loc 1))
-    (looking-at "\n")))
+  (or (= 0 loc)
+      (save-excursion
+        (goto-char (- loc 1))
+        (looking-at "\n"))))
 
 ;;; Debugging
 
@@ -115,5 +137,6 @@
 
 (require 'ruby-mode)
 (define-key ruby-mode-map "\C-cac" 'rrf-extract-constant)
+(define-key ruby-mode-map "\C-cat" 'rrf-extract-temporary)
 (define-key ruby-mode-map "\C-cam" 'rrf-extract-method)
 (define-key ruby-mode-map "\C-cay" 'rrf-insert-extraction)
