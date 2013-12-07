7d2ee2c4f61b09918f25f9c01da729055b7ccbcb
Jim Weirich <jim.weirich@gmail.com>
2013-12-07 08:03:35 -0500

    Delay vertical mode until after init

--- ido.el
+++ ido.el
@@ -1,3 +1,10 @@
 ;;; Put ido in vertical mode
 
-(ido-vertical-mode)
+;;; (require 'ido)
+;;; (require 'ido-vertical-mode)
+;;; (ido-vertical-mode)
+
+
+;;;(esk-eval-after-init '(progn (ido-vertical-mode)))
+
+(add-hook 'after-init-hook '(lambda () (ido-vertical-mode)))
