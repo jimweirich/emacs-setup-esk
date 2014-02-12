1d0dc6a9c969cd445c60a472d45bf92a507a1237
Jim Weirich <jim.weirich@gmail.com>
2014-02-12 13:42:57 -0500

    Use paredit for clojure files.

--- clojure-mode-options.el
+++ clojure-mode-options.el
@@ -3,3 +3,5 @@
           '(lambda ()
              ;;; Override the eval buffer command for clojure mode
              (define-key clojure-mode-map "\C-cv" 'jw-eval-or-clear-buffer)))
+
+(add-hook 'clojure-mode-hook 'paredit-mode)
